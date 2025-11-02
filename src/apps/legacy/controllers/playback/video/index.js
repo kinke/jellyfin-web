dbe507eb537563f79bfe57a6b68bc8d66568de32
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2025-11-02 12:38:57 +0100

    Playback info: Promote out of settings menu to OSD footer icon
    To make it less cumbersome to toggle it on/off. On TV (the only
    platform I've tested this on), this saves about 4 key presses on
    the remote, for each toggling. In particular, closing it takes a
    single OK-press while the OSD is still visible (from toggling it on).

--- src/apps/legacy/controllers/playback/video/index.js
+++ src/apps/legacy/controllers/playback/video/index.js
@@ -971,7 +971,7 @@ export default function (view) {
                     player: player,
                     positionTo: btn,
                     quality: state.MediaSource?.SupportsTranscoding,
-                    stats: true,
+                    stats: false,
                     suboffset: showSubOffset,
                     onOption: onSettingsOption
                 }).finally(() => {
@@ -1772,6 +1772,7 @@ export default function (view) {
         playbackManager.toggleAirPlay(currentPlayer);
     });
     view.querySelector('.btnVideoOsdSettings').addEventListener('click', onSettingsButtonClick);
+    view.querySelector('.btnPlayerStats').addEventListener('click', toggleStats);
     view.addEventListener('viewhide', function () {
         clearHideAnimationEventListeners(headerElement);
         headerElement.classList.remove('hide');
